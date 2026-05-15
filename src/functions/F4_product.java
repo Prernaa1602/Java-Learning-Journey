3725991f2770e2ea625aee3306faed650b233810
Prerna <simple.prerna1602@gmail.com>
2026-05-15 18:38:29 +0530

    product of both numbers

---
+++ src/functions/F4_product.java
@@ -0,0 +1,16 @@
+package functions;
+import java.util.Scanner;
+public class F4_product {
+    public static void main(String [] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("enter the first number  :  ");
+        int first = in.nextInt();
+        System.out.print("enter the second number  :  ");
+        int second = in.nextInt();
+        int res = product(first,second);
+        System.out.println("the product of entered numbers is  :  "+ res);
+    }
+    public static int product(int first, int second){
+        return first*second;
+    }
+}
